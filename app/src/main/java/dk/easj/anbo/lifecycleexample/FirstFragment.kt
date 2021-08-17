a7725f0be9e6bd6ba173ea2ed69d72ac2c7a04ef
Anders Børjesson <anbo@zealand.dk>
2021-08-17 11:00:55 +0200

    Initial commit

---
+++ app/src/main/java/dk/easj/anbo/lifecycleexample/FirstFragment.kt
@@ -0,0 +1,44 @@
+package dk.easj.anbo.lifecycleexample
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import dk.easj.anbo.lifecycleexample.databinding.FragmentFirstBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+    private var _binding: FragmentFirstBinding? = null
+    private val logTag = "fragmentlifecycle"
+
+    // This property is only valid between onCreateView and onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        Log.d(logTag, "onCreateView")
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d(logTag, "onViewCreated")
+        binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.d(logTag, "onDestroyView")
+        _binding = null
+    }
+}
